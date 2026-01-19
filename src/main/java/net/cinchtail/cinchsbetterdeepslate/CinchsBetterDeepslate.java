ced61328f3fed6ba402ef62915fce91aa9fc0084
Joshua Davies <cinchtail@gmail.com>
2026-01-19 00:11:54 -0500

    removed tools, fixed top texture for deepslate wall, tweaks and fixes

--- src/main/java/net/cinchtail/cinchsbetterdeepslate/CinchsBetterDeepslate.java
+++ src/main/java/net/cinchtail/cinchsbetterdeepslate/CinchsBetterDeepslate.java
@@ -6,14 +6,9 @@ import net.cinchtail.cinchsbetterdeepslate.item.ModItems;
 import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
@@ -27,21 +22,8 @@ public class CinchsBetterDeepslate {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
-        modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
 
-    }
-    private void commonSetup(final FMLCommonSetupEvent event) {
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-    }
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-        }
     }
     public static int index = 0;
     public static final CreativeModeTab CINCHSBETTERDEEPSLATE_TAB = new CreativeModeTab("cinchsbetterdeepslate_tab") {
@@ -87,26 +69,6 @@ public class CinchsBetterDeepslate {
             items.set(index, new ItemStack(ModBlocks.POLISHED_DEEPSLATE_PRESSURE_PLATE.get()));
             index++;
             items.set(index, new ItemStack(ModBlocks.POLISHED_DEEPSLATE_BUTTON.get()));
-            index++;
-            items.set(index, new ItemStack(ModItems.DEEPSLATE_SWORD.get()));
-            index++;
-            items.set(index, new ItemStack(ModItems.DEEPSLATE_PICKAXE.get()));
-            index++;
-            items.set(index, new ItemStack(ModItems.DEEPSLATE_AXE.get()));
-            index++;
-            items.set(index, new ItemStack(ModItems.DEEPSLATE_SHOVEL.get()));
-            index++;
-            items.set(index, new ItemStack(ModItems.DEEPSLATE_HOE.get()));
-            index++;
-            items.set(index, new ItemStack(ModItems.BLACKSTONE_SWORD.get()));
-            index++;
-            items.set(index, new ItemStack(ModItems.BLACKSTONE_PICKAXE.get()));
-            index++;
-            items.set(index, new ItemStack(ModItems.BLACKSTONE_AXE.get()));
-            index++;
-            items.set(index, new ItemStack(ModItems.BLACKSTONE_SHOVEL.get()));
-            index++;
-            items.set(index, new ItemStack(ModItems.BLACKSTONE_HOE.get()));
             index++;
         }
     };
