ff2e2f0598d8ad401c86644aa01037be0771e6b3
Joshua Davies <cinchtail@gmail.com>
2026-03-03 13:49:56 -0500

    code cleanup

--- src/main/java/net/cinchtail/cinchsbetterdeepslate/block/ModBlocks.java
+++ src/main/java/net/cinchtail/cinchsbetterdeepslate/block/ModBlocks.java
@@ -1,7 +1,6 @@
 package net.cinchtail.cinchsbetterdeepslate.block;
 
 import net.cinchtail.cinchsbetterdeepslate.CinchsBetterDeepslate;
-import net.cinchtail.cinchsbetterdeepslate.item.ModItems;
 import net.cinchtail.cinchsbetterdeepslate.util.ModBlockSetType;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
@@ -80,20 +79,21 @@ public class ModBlocks {
             () -> new PressurePlateBlock(ModBlockSetType.POLISHED_DEEPSLATE, BlockBehaviour.Properties.ofFullCopy(Blocks.POLISHED_BLACKSTONE_PRESSURE_PLATE).sound(SoundType.POLISHED_DEEPSLATE)));
     public static final RegistryObject<Block> POLISHED_DEEPSLATE_BUTTON = registerBlock("polished_deepslate_button", ModBlocks::modPolishedDeepslateButton);
 
+    public static class ModItems {
+        public static final DeferredRegister<Item> ITEMS =
+                DeferredRegister.create(ForgeRegistries.ITEMS, CinchsBetterDeepslate.MOD_ID);
+    }
+    public static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> registeredBlock = BLOCKS.register(name, block);
+        ModItems.ITEMS.register(name, () -> new BlockItem(registeredBlock.get(), new Item.Properties()));
+        return registeredBlock; }
 
-    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
-        RegistryObject<T> toReturn = BLOCKS.register(name, block);
-        registerBlockItem(name, toReturn);
-        return toReturn;
-    }
-    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
-        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
-    }
     private static Block modPolishedDeepslateButton() {
         return new ButtonBlock(ModBlockSetType.POLISHED_DEEPSLATE, 20, BlockBehaviour.Properties.of().noCollission()
                 .strength(0.5F).pushReaction(PushReaction.DESTROY));
     }
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
+        ModItems.ITEMS.register(eventBus);
     }
 }
