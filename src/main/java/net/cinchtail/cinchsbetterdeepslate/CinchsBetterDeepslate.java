f4ec542554ac957f95f8a38556e779aef2d31be8
Joshua Davies <cinchtail@gmail.com>
2026-03-03 19:11:59 -0500

    small code changes

--- src/main/java/net/cinchtail/cinchsbetterdeepslate/CinchsBetterDeepslate.java
+++ src/main/java/net/cinchtail/cinchsbetterdeepslate/CinchsBetterDeepslate.java
@@ -1,7 +1,7 @@
 package net.cinchtail.cinchsbetterdeepslate;
 
 import net.cinchtail.cinchsbetterdeepslate.block.ModBlocks;
-import net.cinchtail.cinchsbetterdeepslate.item.ModCreativeModeTabs;
+import net.cinchtail.cinchsbetterdeepslate.item.ModItemGroups;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import net.neoforged.bus.api.IEventBus;
@@ -16,6 +16,6 @@ public class CinchsBetterDeepslate {
     public CinchsBetterDeepslate(@NotNull IEventBus bus) {
         ModBlocks.BLOCKS.register(bus);
         ModBlocks.ModItems.ITEMS.register(bus);
-        ModCreativeModeTabs.CREATIVE_MODE_TABS.register(bus);
+        ModItemGroups.CREATIVE_MODE_TABS.register(bus);
     }
 }
