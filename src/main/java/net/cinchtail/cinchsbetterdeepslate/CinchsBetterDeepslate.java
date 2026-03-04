ff87071d8eb53e8db94602e835b792cea5d3c20e
Joshua Davies <cinchtail@gmail.com>
2026-03-03 20:31:52 -0500

    added missing recipe and small fixes

--- src/main/java/net/cinchtail/cinchsbetterdeepslate/CinchsBetterDeepslate.java
+++ src/main/java/net/cinchtail/cinchsbetterdeepslate/CinchsBetterDeepslate.java
@@ -2,7 +2,7 @@ package net.cinchtail.cinchsbetterdeepslate;
 
 import com.mojang.logging.LogUtils;
 import net.cinchtail.cinchsbetterdeepslate.block.ModBlocks;
-import net.cinchtail.cinchsbetterdeepslate.item.ModCreativeModeTabs;
+import net.cinchtail.cinchsbetterdeepslate.item.ModItemGroups;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
@@ -22,7 +22,7 @@ public class CinchsBetterDeepslate {
     public CinchsBetterDeepslate() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModBlocks.register(modEventBus);
-        ModCreativeModeTabs.register(modEventBus);
+        ModItemGroups.register(modEventBus);
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
 
--- src/main/java/net/cinchtail/cinchsbetterdeepslate/item/ModItemGroups.java
+++ src/main/java/net/cinchtail/cinchsbetterdeepslate/item/ModItemGroups.java
@@ -10,7 +10,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
-public class ModCreativeModeTabs {
+public class ModItemGroups {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB,
             CinchsBetterDeepslate.MOD_ID);
 
