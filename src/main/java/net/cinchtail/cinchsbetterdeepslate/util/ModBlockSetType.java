7327c4dabcbfc3a1154f9c5c1334dab1c5233920
Joshua Davies <cinchtail@gmail.com>
2026-03-04 20:21:30 -0500

    icon and tweaks

--- src/main/java/net/cinchtail/cinchsbetterdeepslate/util/ModBlockSetType.java
+++ src/main/java/net/cinchtail/cinchsbetterdeepslate/util/ModBlockSetType.java
@@ -1,6 +1,5 @@
 package net.cinchtail.cinchsbetterdeepslate.util;
 
-
 import it.unimi.dsi.fastutil.objects.ObjectArraySet;
 import net.minecraft.block.BlockSetType;
 import net.minecraft.sound.BlockSoundGroup;
